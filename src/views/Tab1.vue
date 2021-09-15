aa9ab6d4e188e02e0d8dbc56fd0ba5697f5a3ac6
Mateus Raulino <mr.mraulino@gmail.com>
2021-09-15 17:22:02 -0300

    Vue implementado

--- src/views/Tab1.vue
+++ src/views/Tab1.vue
@@ -2,27 +2,63 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Tab1</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true">
+    <ion-content :fullscreen="true" id="app">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title size="large">Tab1</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+      <ion-grid>
+        <ion-row>
+          <p>{{mensagem}}</p>
+          <ion-input v-model="numero" type="num" id="numero" placeholder="Digite um numero de 1 a 10"></ion-input>
+          <ion-button v-on:click="criarTabuada()">Enviar</ion-button>  
+        </ion-row>
+      </ion-grid>
+      <ion-grid>
+        <ul>
+            <li v-for="item in numeros" :key="item">
+                {{ item.num }} x {{ item.mult }} = {{item.result}}
+            </li>
+        </ul>
+      </ion-grid>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonGrid, IonRow, IonInput } from '@ionic/vue';
+import { defineComponent } from 'vue';
 
-export default  {
+export default  defineComponent({
   name: 'Tab1',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
-}
+  components: {IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage, 
+    IonButton, 
+    IonGrid, 
+    IonRow, 
+    IonInput },
+  data() {
+    return {
+      mensagem: 'Insira um numero...',
+      numero: 0,
+      numeros: [{num:0, mult:0, result:0}]
+    }
+  },
+  methods: {
+    criarTabuada(){
+      this.numeros = [{num:0, mult:0, result:0}]
+      for(let i=1; i<=10; i++){
+        this.numeros.push({num:this.numero, mult:i, result:this.numero * i})
+      }
+      console.log(this.numeros)
+    }
+  }
+})
 </script>
